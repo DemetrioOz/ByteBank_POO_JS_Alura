722985c142a00ee4b98c46df191eaa665c62525f
MatheusD <55540506+MatheusDama@users.noreply.github.com>
2022-02-03 16:30:38 -0300

    polimorfismo

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const $ = (message) => console.log([message]);
 
 import { Cliente } from "./Cliente.js";
-import { ContaCorrente } from "./ContaCorrente.js";
-import { ContaPoupanca } from "./ContaPoupanca.js";
+import { ContaCorrente } from "./conta/ContaCorrente.js";
+import { ContaPoupanca } from "./conta/ContaPoupanca.js";
+import { ContaSalario } from "./conta/ContaSalario.js";
 
-const cliente1 = new Cliente("Ricardo", 11122233309);
+import { Gerente } from "./funcionario/Gerente.js";
+import { Diretor } from "./funcionario/Diretor.js";
 
-const contaPoupanca = new ContaPoupanca(cliente1, 1001);
-const contaCorrente = new ContaCorrente(cliente1, 1001);
+import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
 
-$(contaCorrente);
-$(contaPoupanca);
-// console.log(conta1);
+const diretor = new Diretor("Demetrio", 10000, 1234);
+const gerente = new Gerente("Gerente", 5000, 1234);
+
+diretor.cadastrarSenha("123");
+const estaLogado = SistemaAutenticacao.login(diretor, "123");
+
+$(estaLogado);
